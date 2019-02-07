4684c68cac500c900d221744f7a46cdefb96a1cf
Ben Dubuisson <bendubuisson@users.noreply.github.com>
2019-02-08 10:39:06 +1300

    updated sphinxapi file to use a ""modern"" constructor
    Fixes "PHP Deprecated: Methods with the same name" error

--- thirdparty/sphinxapi.php
+++ thirdparty/sphinxapi.php
@@ -397,7 +397,7 @@ class SphinxClient
 	/////////////////////////////////////////////////////////////////////////////
 
 	/// create a new client object and fill defaults
-	function SphinxClient ()
+	function __construct ()
 	{
 		// per-client-object settings
 		$this->_host		= "localhost";
